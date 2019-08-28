85cbe2a29d02e6b9917ae105cee62b63193b2899
rohithyv <53818270+rohithyv@users.noreply.github.com>
2019-08-28 12:44:03 +0530

    Add files via upload

--- prog3a.py
+++ prog3a.py
@@ -1,46 +1,51 @@
-def mergeSort(arr): 
-    if len(arr) >1: 
-        mid = len(arr)//2 
-        L = arr[:mid]  
-        R = arr[mid:]  
-  
-        mergeSort(L)  
-        mergeSort(R)  
-  
+#Python program for implementation of mergesort
+
+def mergesort(arr):
+    if len(arr) >1:
+        mid = len(arr)//2
+        L = arr[:mid]#dividing the array
+        R = arr[mid:]#elements into 2 halves
+
+        mergesort(L) #sorting the first half
+        mergesort(R) #sorting the right half
+
         i = j = k = 0
-          
-         
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
+
+        #copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[i]:
+                arr[k] = L[i]
                 i+=1
-            else: 
-                arr[k] = R[j] 
+            else:
+                arr[k] = R[j]
                 j+=1
             k+=1
-          
-         
-        while i < len(L): 
-            arr[k] = L[i] 
+
+
+            #checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
             i+=1
             k+=1
-          
-        while j < len(R): 
-            arr[k] = R[j] 
+
+        while j < len(R):
+            arr[k] = R[j]
             j+=1
             k+=1
+
+       
+        def printList(arr):
+            for i in range(len(arr)):
+                print(arr[i],end=" ")
+                print()
+
+                if __name__ == '__main__':
+                    arr = [12, 11, 13, 5, 6, 7]
+                    print ("Given array is", end="\n")
+                    printList(arr)
+                    mergeSort(arr)
+                    print("Sorted array is: ", end="\n")
+                    printList(arr) 
   
- 
-def printList(arr): 
-    for i in range(len(arr)):         
-        print(arr[i],end=" ") 
-    print() 
-  
- 
-if __name__ == '__main__': 
-    arr = [22, 33, 78, 90, 12, 5]
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+
+        
